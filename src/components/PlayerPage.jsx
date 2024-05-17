a0b05dced4f7f2ea0bbe1ac156393665324b9857
joker-jonesy <spashmcgrath94@yahoo.com>
2024-05-17 12:25:37 -0400

    kermit

--- src/components/PlayerPage.jsx
+++ src/components/PlayerPage.jsx
@@ -2,6 +2,8 @@ import {useGetPlayerQuery} from "../redux/api/api.js";
 import {useParams, Link} from "react-router-dom";
 import Player from "./Player.jsx";
 import {useDeletePlayerMutation} from "../redux/api/api.js";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrash} from "@fortawesome/free-solid-svg-icons/faTrash";
 
 function PlayerPage(){
 
@@ -14,7 +16,7 @@ function PlayerPage(){
     return(
         <div>
             {isLoading?<h1>Loading...</h1>:<h1><Player data={data.data.player}/></h1>}
-            <button onClick={()=>deletePlayer(id)}>Delete</button>
+            <FontAwesomeIcon style={{color:"red", fontSize:"60px"}} onClick={()=>deletePlayer(id)} icon={faTrash} spin/>
             <Link to={"/"}><button>Return to Players</button></Link>
         </div>
     )
